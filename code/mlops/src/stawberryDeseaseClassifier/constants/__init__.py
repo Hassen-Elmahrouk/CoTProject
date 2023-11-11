761a99a0f5e0b3f8710c6f81c76b72d909fe3120
Hassen-Elmahrouk <zarouihassen0@gmail.com>
2023-11-11 12:21:49 +0100

    'data ingestion'

---
+++ code/mlops/src/stawberryDeseaseClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("/deletable/apps/CoTProject/code/mlops/config/config.yaml")
+PARAMS_FILE_PATH = Path("/deletable/apps/CoTProject/code/mlops/params.yaml")
--- code/mlops/src/stawberryDeseaseClassifier/utils/common.py
+++ code/mlops/src/stawberryDeseaseClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from stawberryDeseaseClassifier.utils import logger
+from src.stawberryDeseaseClassifier.utils import logger
  
 import json
 import joblib
